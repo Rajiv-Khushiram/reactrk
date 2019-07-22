346d826c282c4be418e4ab9d475d5ae50f1bbed3
Rajiv Khushiram <rajivkhushiram@hotmail.com>
2019-07-23 01:07:14 +1000

    heroku deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Creating a CI/CD Dev Environment in 30 mins with Firebase hosting - Github - ReactJSapp  <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<React.Fragment> 
+  <div id="outter">
+
+  </div></React.Fragment>
   );
 }
 
