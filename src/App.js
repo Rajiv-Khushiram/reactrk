97a7e532ab4762656c2b370ee267d151ca25549a
Rajiv Khushiram <rajivkhushiram@hotmail.com>
2019-07-23 19:11:40 +1000

    Added loader js and css-- need to manipulate app.js state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.scss';
 /*import Notification from './components/ClippathNotification'*/
-import NavigationBar from  './components/Navigationbar'
+/* import NavigationBar from  './components/Navigationbar'*/
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  // currently adding loader
   render() {
     return (
       <div className="App">
