3d86a771e1a686fa6ebabfdecbef93eba884d1a7
Rajiv Khushiram <rajivkhushiram@hotmail.com>
2019-07-26 06:02:18 +1000

    cleaning Up

--- src/components/SemanticHomePage/index.js
+++ src/components/SemanticHomePage/index.js
@@ -7,6 +7,7 @@ import './index.css';
 
 
 import {
+    Label,
     Card,
   Button, 
   Transition,
@@ -39,6 +40,50 @@ const getWidth = () => {
 /* Heads up! HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled components for
  * such things.
  */
+
+class HomeHeading2 extends React.Component {
+    state = {
+        visible: false,
+        calculations: {
+          topVisible: false,
+          bottomVisible: false,
+          onScreen: false,
+        }
+        //...whatever construction you need
+       }
+    contextRef = createRef()
+
+    handleUpdate = (e, { calculations }) => this.setState({ calculations })
+
+    render() {
+    const { mobile } = this.props
+    const { calculations } = this.state
+    return (
+        <Visibility onUpdate={this.handleUpdate}
+                updateOn='repaint'
+              >
+      <Transition visible={calculations.onScreen}  animation='zoom' duration={500}>
+      <Container> <Card>
+    <Image src='https://react.semantic-ui.com/images/avatar/large/daniel.jpg' wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>Daniel</Card.Header>
+      <Card.Meta>Joined in 2016</Card.Meta>
+      <Card.Description>
+        Daniel is a comedian living in Nashville.
+      </Card.Description>
+    </Card.Content>
+  </Card> </Container>
+      {/* <Image fluid src='https://www.eenvoud.nl/wp-content/uploads/2017/05/website_bg.png' /> */}
+
+      </Transition>
+ </Visibility>
+       )
+    }
+}
+
+ 
+
+
   class HomepageHeading extends React.Component {
     state = {
         visible: false,
@@ -57,27 +102,13 @@ const getWidth = () => {
     const { mobile } = this.props
     const { calculations } = this.state
     return (
-      <Visibility onUpdate={this.handleUpdate}
-              updateOn='repaint'
-            >
-    <Transition visible={calculations.onScreen}  animation='tada' duration={1500}>
-      <Header
-      as='h1'
-      inverted
-      textAlign="center"
-      content='Full Stack Developer'
-      
-      style={{
-        fontSize: mobile ? '2em' : '4em',
-        fontWeight: 'normal',
-        marginBottom: 0,
-        marginTop: mobile ? '0.0em' : '0em',
-      }}
-    />
-    </Transition>
+        <Visibility onUpdate={this.handleUpdate}
+                updateOn='repaint'
+              >
+      <Transition visible={calculations.onScreen}  animation='bounce' duration={700}>
+        <Image style={{paddingBottom:'10px'}} fluid src='https://www.schoeller-textiles.com/pics/history/history-header-2.jpg' />
+      </Transition>
  </Visibility>
-
-
        )
     }
 }
@@ -133,11 +164,14 @@ class DesktopContainer extends Component {
     return (
       <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
          
-         <Segment
-        style={{padding: '0em', background: 'black' }}
+         <Container
+         id="bg"
+        style={{padding: '0em'}}
       >
-            <HomepageHeading />
-        </Segment>
+            {/* <HomepageHeading /> */}
+            {/* <HomeHeading2 /> */}
+           
+        </Container>
         {children}
       </Responsive>
     )
@@ -181,10 +215,9 @@ class MobileContainer extends Component {
       >
         {/* <MenuNew /> */}
             
-        <Segment
-        style={{background: 'black' }}
-      >                                                                            
+        <Segment>                                                                            
         <HomepageHeading mobile />
+        <HomeHeading2 />
         </Segment>
           {children}
       </Responsive>
@@ -236,47 +269,75 @@ class HomepageLayout extends Component {
         
     
         return (  <ResponsiveContainer>  
-         
-            <Grid celled>
-    <Grid.Row>
-      <Grid.Column width={3}>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Column width={13}>
-        <Image src='https://react.semantic-ui.com/images/wireframe/centered-paragraph.png' />
-      </Grid.Column>
-    </Grid.Row>
-
-    <Grid.Row>
-      <Grid.Column width={3}>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Column width={10}>
-        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
-      </Grid.Column>
-      <Grid.Column width={3}>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Row>
-    </Grid.Row>
-    </Grid.Row>
-    <Grid.Row>
-      <Grid.Column width={3}>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Column width={13}>
-        <Image src='https://react.semantic-ui.com/images/wireframe/centered-paragraph.png' />
-      </Grid.Column>
-    </Grid.Row>
-    <Grid.Row>
-      <Grid.Column width={3}>
-        <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-      </Grid.Column>
-      <Grid.Column width={13}>
-        <Image src='https://react.semantic-ui.com/images/wireframe/centered-paragraph.png' />
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
+    <Segment style={{ padding: '8em 0em' }} vertical>
+      <Container text>
+        <Header as='h3' style={{ fontSize: '2em' }}>
+          Breaking The Grid, Grabs Your Attention
+        </Header>
+        <p style={{ fontSize: '1.33em' }}>
+          Instead of focusing on content creation and hard work, we have learned how to master the
+          art of doing nothing by providing massive amounts of whitespace and generic content that
+          can seem massive, monolithic and worth your attention.
+        </p>
+        <Button as='a' size='large'>
+          Read More
+        </Button>
+        <Divider
+          as='h4'
+          className='header'
+          horizontal
+          style={{ margin: '3em 0em', textTransform: 'uppercase' }}
+        >
+          <a href='#'>Case Studies</a>
+        </Divider>
+        <Header as='h3' style={{ fontSize: '2em' }}>
+          Did We Tell You About Our Bananas?
+        </Header>
+        <p style={{ fontSize: '1.33em' }}>
+          Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but
+          it's really true. It took years of gene splicing and combinatory DNA research, but our
+          bananas can really dance.
+        </p>
+        <Button as='a' size='large'>
+          I'm Still Quite Interested
+        </Button>
+      </Container>
+    </Segment>
+    <Segment inverted vertical style={{ padding: '5em 0em' }}>
+      <Container>
+        <Grid divided inverted stackable>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='About' />
+              <List link inverted>
+                <List.Item as='a'>Sitemap</List.Item>
+                <List.Item as='a'>Contact Us</List.Item>
+                <List.Item as='a'>Religious Ceremonies</List.Item>
+                <List.Item as='a'>Gazebo Plans</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Services' />
+              <List link inverted>
+                <List.Item as='a'>Banana Pre-Order</List.Item>
+                <List.Item as='a'>DNA FAQ</List.Item>
+                <List.Item as='a'>How To Access</List.Item>
+                <List.Item as='a'>Favorite X-Men</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={7}>
+              <Header as='h4' inverted>
+                Footer Header
+              </Header>
+              <p>
+                Extra space for a call to action inside the footer that could help re-engage users.
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </Segment>
+           
           </ResponsiveContainer> )
     }
         }
