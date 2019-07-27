9fc8e5d509092882fd10e8a8a14cf8e1f8bc6dcf
thuy.pham <thuy.pham@xero.com>
2019-07-27 20:23:45 +1000

    add the congratulation page

--- src/components/DemoSuccess/index.js
+++ src/components/DemoSuccess/index.js
@@ -1,10 +1,20 @@
 import React, {Component } from 'react';
+import ButtonExamplePositive from './button';
+import SearchExampleCategory from './searchbar';
+import './index.scss';
 
 class Success extends Component{
     render(){
         return (
-            <div>
-                <h1>Success</h1>
+            <div >
+                <div className='searchbar'><SearchExampleCategory /></div>
+                <div><h1 className='assignmentheader'>Assignment 1: APPLE</h1></div>
+                <hr/>
+                <div className='text'> 
+                    <h1 >Congratulations! You have finished all of the required steps </h1>
+                    <ButtonExamplePositive />
+                </div>
+                <hr/>
             </div>
         )
     }
